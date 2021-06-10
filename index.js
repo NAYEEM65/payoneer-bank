0f0b56ace49f701d950b6a9d407748e0344d8f41
NAYEEM65 <70934241+NAYEEM65@users.noreply.github.com>
2021-06-10 10:35:03 +0600

    updated all files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//login button handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area");
@@ -5,3 +6,57 @@ loginBtn.addEventListener("click", function(){
     const transectionArea = document.getElementById("transection-area")
     transectionArea.style.display = "block";
 });
+
+// deposit buton event handeler
+
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+    
+    // const currentDeopsit = document.getElementById("currentDeposit").innerText;
+    // const currentDepositNumber = parseFloat(currentDeopsit);
+    // const totalDeposit = depositNumber + currentDepositNumber;
+
+    // document.getElementById("currentDeposit").innerText = totalDeposit;
+
+    // const currentBalance = document.getElementById("currentBalance").innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // const totalBalance = currentBalanceNumber + totalDeposit;
+    // document.getElementById("currentBalance").innerText = totalBalance;
+    updateSpanText("currentDeposit", depositNumber)
+    updateSpanText("currentBalance", depositNumber)
+    if(depositAmount==""){
+        alert("please enter a valid amount");
+    }
+
+    document.getElementById("depositAmount").value = null;
+});
+
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = currentNumber + depositNumber;
+    document.getElementById(id).innerText = totalAmount;    
+}
+
+//withdraw button event handeler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawAmount = document.getElementById("withdrawAmount").value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const currentWithdraw = document.getElementById("currentWithdraw").innerText;
+    const currentWithdrawNumber = parseFloat(currentWithdraw);
+    const totalWithdraw = currentWithdrawNumber + withdrawNumber;
+    document.getElementById("currentWithdraw").innerText = totalWithdraw;
+
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = currentBalanceNumber - totalWithdraw;
+    if(currentBalanceNumber<totalWithdraw){
+        alert("insuficient Balance 😥")
+    }
+    document.getElementById("currentBalance").innerText = totalBalance;
+    document.getElementById("withdrawAmount").value = "";
+})
